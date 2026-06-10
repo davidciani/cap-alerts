c96692907d39719c5c64f2ff3ae7e884027b4cb8
David Ciani <dciani@davidciani.com>
2026-06-09 18:51:44 -0700

    feat: add capability to load directly from the api.

--- src/cap_alerts/load_alerts.py
+++ src/cap_alerts/load_alerts.py
@@ -5,13 +5,16 @@ import lzma
 from pathlib import Path
 from typing import TYPE_CHECKING
 
+import httpx
 import msgspec.json
 from lxml import etree
 from rich.console import Console
+from rich.logging import RichHandler
 from rich.progress import (
     BarColumn,
     MofNCompleteColumn,
     Progress,
+    ProgressColumn,
     SpinnerColumn,
     TaskProgressColumn,
     TextColumn,
@@ -27,20 +30,30 @@ if TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 
-IN_DIR = Path("data/ipaws_alerts/json")
-FILES = list(IN_DIR.glob("IpawsArchivedAlerts_*.jsonl.xz"))
+MODE = "api"  # api or files
 
-DATABASE_URL = "postgresql+psycopg://cap_alerts_app@localhost/cap_alerts_dev"
+# paths for files mode
+IN_DIR = Path("data/ipaws_alerts/json")
+PATTERN = "IpawsArchivedAlerts_*.jsonl.xz"
+
+# url for api mode
+FEMA_API_URL = "https://www.fema.gov/api/open/v1/IpawsArchivedAlerts"
+
+
+DATABASE_URL = "postgresql+psycopg://cap_alerts_app@localhost/cap_alerts"
 # DATABASE_URL = "sqlite:///:memory:"
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s %(levelname)s %(name)s %(message)s",
-)
+engine = create_engine(DATABASE_URL, echo=False)
 
 console = Console()
 
-progress_columns = [
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler(console=console)]
+)
+
+
+progress_columns: list[ProgressColumn] = [
     SpinnerColumn(),
     TextColumn("[progress.description]{task.description}"),
     BarColumn(),
@@ -48,9 +61,6 @@ progress_columns = [
     TaskProgressColumn(),
     TimeRemainingColumn(),
 ]
-
-
-session: Session
 
 
 def has_cmas(alert: models.Alert) -> bool:
@@ -96,7 +106,67 @@ def load_from_file(file_path: Path, progress: Progress | None = None):
             progress.update(task_id, visible=False)
 
 
-engine = create_engine(DATABASE_URL, echo=False)
+def load_from_files(directory: Path, pattern: str, progress: Progress | None):
+    files_sorted = sorted(directory.glob(pattern), reverse=True)
+
+    if progress:
+        task_id = progress.add_task("Loading files…")
+        files_sorted = progress.track(files_sorted, task_id=task_id)
+
+    for file_path in files_sorted:
+        load_from_file(file_path, progress=progress)
+
+
+def load_from_api(progress: Progress | None = None):
+
+    with Session(engine) as session, httpx.Client(timeout=None) as client:
+        if progress:
+            task_id = progress.add_task("Loading records…")
+            need_count = True
+        else:
+            need_count = False
+
+        # Iterate until no more pages of results
+        skip = 0
+        while True:
+            # Get page of results from API
+            try:
+                resp = client.get(
+                    FEMA_API_URL,
+                    params={"$skip": str(skip), "$top": 10_000, "$count": need_count},
+                )
+                resp.raise_for_status()
+
+            except httpx.HTTPStatusError as e:
+                console.log(f"ERROR {e.response.status_code}: {e.response.url}")
+                raise
+
+            # Decode json
+            json_data = resp.json()
+
+            if progress and need_count:
+                progress.update(task_id, total=int(json_data["metadata"]["count"]))
+                need_count = False
+
+            # If no results were returned, we're done.
+            if len(json_data["IpawsArchivedAlerts"]) == 0:
+                break
+
+            for alert_data in json_data["IpawsArchivedAlerts"]:
+                raw_xml: str = alert_data["originalMessage"]
+                root = etree.fromstring(raw_xml.encode())
+                alert = extract_alert(root)
+
+                # skip the non-CMAS alerts from NWS
+                if alert.sender == "w-nws.webmaster@noaa.gov" and not has_cmas(alert):
+                    pass
+                else:
+                    session.add(alert)
+
+                if progress:
+                    progress.update(task_id, advance=1)
+
+            session.commit()
 
 
 def main() -> None:
@@ -105,18 +175,12 @@ def main() -> None:
     SQLModel.metadata.drop_all(bind=engine)
     SQLModel.metadata.create_all(engine)
 
-    session = Session(engine)
-
     with Progress(*progress_columns, console=console) as progress:
-        files = sorted(FILES, reverse=True)
-        overall_progress_task = progress.add_task(
-            "Loading files…",
-            total=len(files),
-        )
-
-        for file_path in sorted(files):
-            load_from_file(file_path, progress=progress)
-            progress.update(overall_progress_task, advance=1)
+        match MODE:
+            case "files":
+                load_from_files(IN_DIR, PATTERN, progress=progress)
+            case "api":
+                load_from_api(progress=progress)
 
     console.log("END")
 
